bf8a7a9bf6e5f9beaf1797eb35ceb651b5a0d6ed
carlosherrero-edu <carlosherrero.edu@gmail.com>
2021-01-28 15:59:47 +0100

    Añadida clase de prueba de ConversoresTemperaturas

---
+++ test/pruebasunitarias/ConversoresTemperaturasTest.java
@@ -0,0 +1,27 @@
+package pruebasunitarias;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConversoresTemperaturasTest {
+
+	final double ERROR=0.001;
+
+	@Test
+	void testConversorTemperaturask() {
+		
+		
+		assertEquals(32.0, ConversoresTemperaturas.conversorTemperaturas(0,'c'), ERROR);
+		assertEquals(32.0, ConversoresTemperaturas.conversorTemperaturas(0,'C'),ERROR);
+	}
+	
+	@Test
+	void testConversorTemperaturasf() {
+		
+		
+		assertEquals(100.0, ConversoresTemperaturas.conversorTemperaturas(212.0, 'f'), ERROR);
+		assertEquals(100.0, ConversoresTemperaturas.conversorTemperaturas(212.0, 'F'), ERROR);
+	}
+
+}
